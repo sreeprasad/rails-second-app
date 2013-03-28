e17844e4fc8f434f082281ede73b91c02a17f7f3
sreeprasad <sreeprasad.sp@gmail.com>
2013-03-28 19:34:10 -0400

    first commit , simple skeleton now

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'bb5229a6aabd5ad7376aa9498b6191d94510260c5941fcce60833f6f82dadd969a0a12408a5eb1977f61fd487b4edc3d9a2d2a05ff4483554d91c0aedfe6b56c'
---
+++ readme.md
@@ -0,0 +1,4 @@
+This is my second app on ruby.
+Extending the blogging application further, this application includes
+static and dynamic pages implementing a full stack of features such as 
+authentication, blogging and hopefully picture uploads.
